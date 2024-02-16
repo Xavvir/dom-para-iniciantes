e9bb87d9cfe15817021dd329545431bacde8fa4b
Xavvir <leandroxm1997@hotmail.com>
2024-02-16 19:59:13 -0300

    inicial

---
+++ 0301-o-que-e-o-dom/script.js
@@ -0,0 +1,13 @@
+const urlPagina = window.location.href
+
+console.log('O link atual é', urlPagina)
+
+const buscaClasseAtiva = document.getElementsByClassName('ativo')
+const buscaClasseAtiva1 = document.querySelector('.ativo') //querySelector busca o primeiro elemento
+
+console.log(buscaClasseAtiva1)
+console.log(buscaClasseAtiva)
+
+const tamanhoTela = window.screen.width
+
+console.log(tamanhoTela)
